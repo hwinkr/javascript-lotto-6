a0657712ab530385b798cc15e59d3b3f3dd0ff76
hwinkr <dnddl8280@naver.com>
2023-12-13 12:48:48 +0900

    chore(add constants): 게임 결과 메시지 추가

--- src/constants/bridge-game.js
+++ src/constants/bridge-game.js
@@ -2,6 +2,8 @@ export const GAME_MESSAGES = {
   start: '다리 건너기 게임을 시작합니다.',
   success: '성공',
   failed: '실패',
+  result: '최종 게임 결과',
+  succeedOrFailed: '게임 성공 여부',
   totalTryCount: (tryCount) => `총 시도한 횟수 : ${tryCount}`,
 };
 
