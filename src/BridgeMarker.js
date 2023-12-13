95598a9c74e69d37c4e010d3880463e2edffe10c
hwinkr <dnddl8280@naver.com>
2023-12-13 10:44:27 +0900

    feat(BridgeMaker): 다리를 생성하는 객체 구현
    - 다리를 생성할 때, 다리의 길이에 대한 유효성도 검증하도록 구현

---
+++ src/BridgeMarker.js
@@ -0,0 +1,34 @@
+/**
+ * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
+ */
+
+import { BRIDGE_PATH, BRIDGE_PATH_NUMBER, BRIDGE_SIZE_RANGE, ERROR_MESSAGES } from './constants/bridge-game';
+import InvalidInputException from './exceptions/InvalidInputException';
+
+const BridgeMaker = {
+  validateBridgeSize(size) {
+    if (!typeof size === 'number' || Number.isNaN(size)) {
+      throw new InvalidInputException(ERROR_MESSAGES.invalidBridgeSize);
+    }
+    if (size < BRIDGE_SIZE_RANGE.min || size > BRIDGE_SIZE_RANGE.max) {
+      throw new InvalidInputException(ERROR_MESSAGES.invalidBridgeSize);
+    }
+  },
+  /**
+   * @param {number} size 다리의 길이
+   * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
+   * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
+   */
+  makeBridge(size, generateRandomNumber) {
+    this.validateBridgeSize(size);
+    const bridge = Array.from({ length: size }).reduce((accBridge) => {
+      const randomNumber = generateRandomNumber();
+      const path = randomNumber === BRIDGE_PATH_NUMBER.down ? BRIDGE_PATH.down : BRIDGE_PATH.up;
+      return [...accBridge, path];
+    }, []);
+
+    return bridge;
+  },
+};
+
+export default BridgeMaker;
